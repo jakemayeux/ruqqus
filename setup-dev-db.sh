e3d5ec4a80b73c0341a50ae282b7720e6eb1bd0a
Jacob Mayeux <jakemayeux@gmail.com>
2020-06-30 22:19:42 -0400

    update

--- setup-dev-db.sh
+++ setup-dev-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql postgres -h localhost -U postgres -a -f schema.txt
+psql postgres -h localhost -U postgres -f schema.txt
  
-psql postgres -h localhost -U postgres -a -f dev-schema.txt
+psql postgres -h localhost -U postgres -f dev-schema.txt
 
